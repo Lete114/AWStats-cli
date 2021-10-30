e113f30446d0773326cef8d8d6ea2da8afaa00b6
lete114 <lete@lete114.top>
2021-10-30 12:04:18 +0800

    移除simple-git库，改用cross-spawn库进行操作 移除部分方法，改用cross-spawn库操作 调整版本号显示样式(显示cli版本和程序版本号) 若干微调

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,5 @@
-const { join, resolve } = require('path')
-const fs = require('fs')
+const { join } = require('path')
+const { writeFileSync } = require('fs')
 
 /**
  *  获取绝对路径
@@ -11,39 +11,18 @@ function resolvePath(...Path) {
   return join(root_dir, ...Path)
 }
 
-/**
- * 复制文件
- * @param {*} sourcesPath 原文件路径
- * @param {*} attachPath 指定输出路径
- */
-function fileCopy(sourcesPath, attachPath) {
-  fs.stat(sourcesPath, (err, stat) => {
-    if (err) throw err
-    if (stat.isFile()) {
-      // 创建读取流
-      let readable = fs.createReadStream(sourcesPath)
-      // 创建写入流
-      let writable = fs.createWriteStream(attachPath)
-      readable.on('data', (chunk) => writable.write(chunk))
-      readable.on('end', () => writable.end())
-    }
-  })
+function DeepClone(obj = {}) {
+  const str = JSON.stringify(obj)
+  return JSON.parse(str)
 }
 
-/**
- * 删除目录下的所有文件及目录，包括当前目录
- * @param path 需要删除的路径文件夹
- */
-function removeAll(_path) {
-  if (!fs.existsSync(_path)) return
-  const files = fs.readdirSync(_path)
-  for (let item of files) {
-    const file_path = resolve(`${_path}/${item}`)
-    const stats = fs.statSync(file_path)
-    if (stats.isDirectory()) removeAll(file_path)
-    else fs.unlinkSync(file_path) //删除文件
-  }
-  fs.rmdirSync(_path) // 删除文件夹
+function SetVersion() {
+  const pkgPath = resolvePath('package.json')
+  const pkg = require(pkgPath)
+  const awstatsVersion = pkg.dependencies.awstats || ''
+  const version = awstatsVersion.replace(/\^|\~|\*/, '')
+  pkg.awstats.version = version
+  writeFileSync(pkgPath, JSON.stringify(pkg, null, 2), { encoding: 'utf-8' })
 }
 
-module.exports = { removeAll, fileCopy, resolvePath }
+module.exports = { resolvePath, DeepClone, SetVersion }
